b51971034faab03809dc5645e71fcb6e656a8695
Daniel Kończyk <daniel@konczyk.net>
2016-05-09 00:17:19 +0200

    Add client to solve puzzles read from stdin

--- README.md
+++ README.md
@@ -7,3 +7,33 @@ Solve the 8-puzzle problem using the A* search algorithm.
 Build project:
 
     $ ./gradlew assemble
+
+Try to solve a solvable puzzle read for a standard input:
+
+    $ cat data/puzzle3.txt | java -cp build/libs/8puzzle.jar PuzzleClient -
+    Minimum number of moves: 3
+
+    3
+     1  2  3
+     0  4  5
+     7  8  6
+
+    3
+     1  2  3
+     4  0  5
+     7  8  6
+
+    3
+     1  2  3
+     4  5  0
+     7  8  6
+
+    3
+     1  2  3
+     4  5  6
+     7  8  0
+
+Try to solve an unsolvable puzzle read for a standard input:
+
+    $ cat data/puzzle3-unsolvable.txt | java -cp build/libs/8puzzle.jar PuzzleClient -
+    Puzzle is unsolvable
---
+++ src/PuzzleClient.java
@@ -0,0 +1,98 @@
+import com.beust.jcommander.IParameterValidator;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import java.util.Scanner;
+
+public class PuzzleClient {
+
+    @Parameter(
+        names = {"--size", "-s"},
+        description = "Random puzzle size (2 - 127)",
+        validateWith = BoardSizeValidator.class)
+    private int size;
+
+    @Parameter(
+        names = {"--help", "-h"},
+        description = "Usage help",
+        help = true)
+    private boolean help = false;
+
+    @Parameter(
+        names = {"--stdin", "-"},
+        description = "Read data from stdin "
+            + "(board size first, followed by block rows)")
+    private boolean stdin = false;
+
+    public static class BoardSizeValidator implements IParameterValidator {
+        @Override
+        public void validate(String name, String value) throws ParameterException {
+            String msg = "Parameter --size should be a positive integer"
+                + "between 2 and 127 (found " + value + ")";
+            try {
+                int n = Integer.parseInt(value);
+                if (n < 2 || n > 127) {
+                    throw new ParameterException(msg);
+                }
+            } catch (Exception e) {
+                throw new ParameterException(msg);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PuzzleClient client = new PuzzleClient();
+        JCommander jc = new JCommander(client);
+        try {
+            jc.parse(args);
+            client.validate();
+            if (client.help || args.length == 0) {
+                jc.usage();
+                return;
+            }
+            client.run();
+        } catch (ParameterException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void validate() throws ParameterException {
+        if (stdin && size > 0) {
+            throw new ParameterException(
+                "Parameters --stdin and --size are mutually exclusive");
+        }
+    }
+
+    private void run() {
+        Board initial = null;
+        if (stdin) {
+            initial = new Board(loadBoardFromStdIn());
+        }
+        Solver solver = new Solver(initial);
+
+        if (!solver.isSolvable()) {
+            System.out.println("Puzzle is unsolvable");
+        } else {
+            System.out.println("Minimum number of moves: "
+                                + solver.moves() + "\n");
+            for (Board board : solver.solution()) {
+                System.out.println(board);
+            }
+        }
+    }
+
+    private int[][] loadBoardFromStdIn() {
+        int[][] blocks;
+        try (Scanner scanner = new Scanner(System.in)) {
+            int size = scanner.nextInt();
+            blocks = new int[size][size];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    blocks[i][j] = scanner.nextInt();
+                }
+            }
+        }
+
+        return blocks;
+    }
+}
